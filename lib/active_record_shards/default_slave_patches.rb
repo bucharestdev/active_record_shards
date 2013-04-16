e0a05e299a3de5645dd150b114d6f43ef218a9d3
bucharestdev <raj_dasari@yahoo.com>
2013-04-16 22:32:06 +0530

    trimmed some code

--- lib/active_record_shards/default_slave_patches.rb
+++ lib/active_record_shards/default_slave_patches.rb
@@ -1,7 +1,7 @@
-module ActiveRecordShards
-  module DefaultSlavePatches
-    CLASS_SLAVE_METHODS = [ :find_by_sql, :count_by_sql, :calculate, :find_one, :find_some, :find_every, :quote_value, :sanitize_sql_hash_for_conditions ]
-
+ module ActiveRecordShards
+   module DefaultSlavePatches
+    #CLASS_SLAVE_METHODS = [ :find_by_sql, :count_by_sql, :calculate, :find_one, :find_some, :find_every, :quote_value, :sanitize_sql_hash_for_conditions ]
+    CLASS_SLAVE_METHODS = [ ]
     def self.extended(base)
       base_methods = (base.methods | base.private_methods).map(&:to_sym)
       (CLASS_SLAVE_METHODS & base_methods).each do |slave_method|
@@ -20,9 +20,9 @@ module ActiveRecordShards
 
       base.class_eval do
         # fix ActiveRecord to do the right thing, and use our aliased quote_value
-        def quote_value(*args, &block)
-          self.class.quote_value(*args, &block)
-        end
+        # def quote_value(*args, &block)
+        #   self.class.quote_value(*args, &block)
+        # end
 
         def reload_with_slave_off(*args, &block)
           self.class.on_master { reload_without_slave_off(*args, &block) }
@@ -43,47 +43,32 @@ module ActiveRecordShards
         end
 
         class << self
-          def transaction_with_slave_off(*args, &block)
-            if on_slave_by_default?
-              old_val = Thread.current[:_active_record_shards_slave_off]
-              Thread.current[:_active_record_shards_slave_off] = true
-            end
+        #   def transaction_with_slave_off(*args, &block)
+        #     if on_slave_by_default?
+        #       old_val = Thread.current[:_active_record_shards_slave_off]
+        #       Thread.current[:_active_record_shards_slave_off] = true
+        #     end
 
-            transaction_without_slave_off(*args, &block)
-          ensure
-            if on_slave_by_default?
-              Thread.current[:_active_record_shards_slave_off] = old_val
-            end
-          end
+        #     transaction_without_slave_off(*args, &block)
+        #   ensure
+        #     if on_slave_by_default?
+        #       Thread.current[:_active_record_shards_slave_off] = old_val
+        #     end
+        #   end
 
-          alias_method_chain :transaction, :slave_off
+        #   alias_method_chain :transaction, :slave_off
 
 
-          def table_exists_with_default_slave?(*args)
-            on_slave_unless_tx(*args) { table_exists_without_default_slave?(*args) }
-          end
+        #   def table_exists_with_default_slave?(*args)
+        #     on_slave_unless_tx(*args) { table_exists_without_default_slave?(*args) }
+        #   end
 
-          alias_method_chain :table_exists?, :default_slave
-        end
+        #   alias_method_chain :table_exists?, :default_slave
+         end
       end
 
 
-      ActiveRecord::Associations::HasAndBelongsToManyAssociation.class_eval do
-        def construct_sql_with_default_slave(*args, &block)
-          on_slave_unless_tx do
-            construct_sql_without_default_slave(*args, &block)
-          end
-        end
-
-        def construct_find_options_with_default_slave!(*args, &block)
-          on_slave_unless_tx do
-            construct_find_options_without_default_slave!(*args, &block)
-          end
-        end
-
-        alias_method_chain :construct_sql,           :default_slave if respond_to?(:construct_sql)
-        alias_method_chain :construct_find_options!, :default_slave if respond_to?(:construct_find_options!)
-      end
+      
     end
 
     def on_slave_unless_tx(&block)
@@ -94,4 +79,4 @@ module ActiveRecordShards
       end
     end
   end
-end
+ end
